0e0a45a7d3a7345a1ed0430ac343dee8447f7223
bergmpe <bergmpe@hotmail.com>
2017-11-23 07:36:03 -0200

    create function. this function receive te path of image, shows the gray scale image read and the negative image. It also returns the negative image.

--- negative.m
+++ negative.m
@@ -1,6 +1,11 @@
-color_img = imread('lena.jpg');
-gray_img = rgb2gray(color_img);
+function negative_img = negative(image_path)
+    gray_img = load_img(image_path);
+    L = max( gray_img(:) );
+    negative_img = L - gray_img; 
+    imshow( [gray_img, negative_img] );
+end
 
-L = max( gray_img(:) );
-negative_img = L - gray_img; 
-imshow( [gray_img, negative_img] );
+function gray_img = load_img(image_path)
+    original_img = imread(image_path);
+    gray_img = rgb2gray(original_img);
+end
