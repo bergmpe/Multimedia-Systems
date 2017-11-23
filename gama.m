6f9e6c4ede1fb6d082fe50ea3b45bbbde9b1b021
bergmpe <bergmpe@hotmail.com>
2017-11-23 08:29:45 -0200

    create function. The function shows the gama transformed image and the gray scaled image. It also returns the gama transformaded image.

--- gama.m
+++ gama.m
@@ -1,10 +1,14 @@
-color_img = imread('lena.jpg');
-gray_img = rgb2gray(color_img);
+function gama_img = gama(image_path, c, y)
+    gray_img =load_img(image_path);
+    L = max( gray_img(:) );
+    gama_img = double(gray_img * c) .^ y;
+    L_gama = max( gama_img(:));
+    factor =  double(L) / L_gama;
+    gama_img = gama_img * factor;
+    imshow( [gray_img, gama_img] );
+end
 
-L = max( gray_img(:) );
-gama_img = double(gray_img) .^ 1.5;
-L_gama = max( gama_img(:));
-factor =  double(L) / L_gama;
-gama_img = gama_img * factor;
-
-imshow( [gray_img, gama_img] );
+function gray_img = load_img(image_path)
+    original_img = imread(image_path);
+    gray_img = rgb2gray(original_img);
+end
