b00efde556b34a9137c402621db1229382688876
bergmpe <bergmpe@hotmail.com>
2017-12-13 21:34:28 -0200

    apply wavelet transform to image and rebuild the image.

--- wavelet_compress.m
+++ wavelet_compress.m
@@ -1,38 +1,46 @@
-function [CA] = wavelet_compress(image_name, compress)
-    CAS = {};
+function [CA RE] = wavelet_compress(image_name, compress)
     CHS = {};
     CVS = {};
     CDS = {};
     original_image = load_img(image_name);
-    current_img = original_image;
-    [r, c] = size( current_img );
-    i = 1;
+    CA = original_image;
+    [r, c] = size( CA );
+    counter = 1;
     
-    while c >= 8 && r >= 8
-        [CA, CH, CV, CD] = dwt2(current_img, 'haar');
-        current_img = CA;
+    while c > 32 && r > 32
+        [CA, CH, CV, CD] = dwt2(CA, 'haar');
+        %current_img = CA./2;
         
-        CA = round(CA);
-        CH = round(CH);
-        CV = round(CV);
-        CD = round(CD);
+        CA = round(CA./2);
+        CH = round(CH./2);
+        CV = round(CV./2);
+        CD = round(CD./2);
         %apply limiar to CH, CV, CD
         CH = limiar(CH, compress);
         CV = limiar(CV, compress);
         CD = limiar(CD, compress);
         
-        CAS(:,:, i) = {CA./2};
-        CHS(:,:, i) = {CH./2};
-        CVS(:,:, i) = {CV./2};
-        CDS(:,:, i) = {CD./2};
+        CHS(:,:, counter) = {CH};
+        CVS(:,:, counter) = {CV};
+        CDS(:,:, counter) = {CD};
         
         %symbols_probabilites = tabulate( round(CH(:)./ 2) );
         %dict = huffmandict( symbols_probabilites(:,1), (symbols_probabilites(:, 3)/ 100) );
-        i = i + 1;
-        [r, c] = size( current_img );
+        counter = counter + 1;
+        [r, c] = size( CA );
     end
+    counter = counter - 1;
     
-    imshow( uint8(CA ./ 32));
+    
+    for i = counter:-1:1
+        disp(i)
+        CA = idwt2( CA.*2, cell2mat(CHS(:,:, i)).*2, cell2mat(CVS(:,:, i)).*2, cell2mat(CDS(:,:, i)).*2,'haar');
+        RE = cell2mat(CDS(:,:, i)).*2;
+        disp(RE);
+    end
+    imshow([original_image, uint8(CA)]);
+    %imshow(uint8(CA));
+    
 end
 
 function img = load_img(image_name)
